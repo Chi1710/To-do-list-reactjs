f136d539b98949ab1c7f41554f2c03f3eff0c1c1
Chi <chiyantsestudio@gmail.com>
2022-11-15 21:23:28 -0500

    finish add tasks to the list and css styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import {
-//   faCircleCheck, faPen, faTrashCan
-// } from '@fortawsome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
 
 import './App.css';
 
@@ -22,7 +23,12 @@ function App() {
 
   //Add task
   const addTask = () => {
-
+    if(newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false}
+      setToDo([...toDo, newEntry ])
+      setNewTask('');
+    }
 
   }
 
@@ -67,21 +73,72 @@ function App() {
       {toDo && toDo.length ? '' : 'No Task...'}
 
       {toDo && toDo
+      
+        // .sort((a,b) => a.id >b.id ? 1 : -1)
+        // the order of tasks will always start from the smallest id to larger 
         .map((task, index) => {
           return(
+            <React.Fragment key={task.id}>
             <div className='col taskBg'>
-            <div className={task.status ? 'done' : ''}>
 
-              <React.Fragment key={task.id}>
+            <div className={task.status ? 'done' : ''}>
                 <span className='taskNumber'> {index + 1}</span>
                 <span className='taskText'> {task.title}</span>
-              </React.Fragment>
-
             </ div>
+
+            <div className="iconsWrap">
+              <span title="Completed / Not Completed">
+                <FontAwesomeIcon icon={faCircleCheck} />
+              </span>
+              <span title="Edit">
+              <FontAwesomeIcon icon={faPen} />
+              </span>
+              <span title="Delete">
+              <FontAwesomeIcon icon={faTrashCan} />
+              </span>           
             </div>
+
+
+                      
+            </div>
+            </React.Fragment>
+
           )
         })
       }
+            {/* Update Task*/}
+            <div className='row'>
+              <div className='col'>
+                <input className='form-control form-control-lg'/>
+              </div>
+              <div className='col-auto'>
+                <button className='btn btn-lg btn-success mr-20'>
+                  Update
+                </button >
+                <button className='btn btn-lg btn-warning'>
+                  Cancel
+                  </button>
+              </div>              
+            </div>
+            <br />
+
+
+            {/* Add Task*/}
+            <div className='row'>
+              <div className='col'>
+                <input 
+                value={newTask}
+                onChange={ (e) => setNewTask(e.target.value)}
+                className='form-control form-control-lg'/>
+              </div>
+              <div className='col-auto'>
+                <button
+                onClick = {addTask} 
+                className='btn btn-lg btn-success'>
+                  Add Task
+                </button>
+              </div>              
+            </div>
 
 
     </div>
